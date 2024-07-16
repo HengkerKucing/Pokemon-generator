a663a9ac590e618417867cc269825ad2bf24a016
HengkerKucing <140718591+HengkerKucing@users.noreply.github.com>
2024-07-16 08:20:24 +0700

    fetch data melalui api dan menampilkannya

---
+++ script.js
@@ -0,0 +1,66 @@
+bug: "#26de81",
+dragon: "#ffeaa7",
+electric: "#fed330",
+fairy: "#FF0069",
+fighting: "#30336b",
+fire: "#f0932b",
+flying: "#"
+
+const url = "https://pokeapi.co/api/v2/pokemon/"
+const card = document.getElementById("card")
+const btn = document.getElementById("btn")
+
+let getPokeData = () => {
+    let id = Math.floor(Math.random() * 150) + 1
+    const finalUrl = url + id
+    fetch(finalUrl).then((response) => response.json()).then((data) => {
+        generateCard(data)
+    })
+}
+
+// Generate card
+let generateCard = (data) => {
+    //get necessary data and assign it to variables
+    console.log(data)
+    const hp = data.stats[0].base_stat
+    const imgSrc = data.sprites.other.dream_world.front_default
+    const pokeName = data.name
+    const statAttack = data.stats[1].base_stat
+    const statDeffense = data.stats[2].base_stat
+    const statSpeed = data.stats[5].base_stat
+
+    card.innerHTML = `
+            <p class="hp">
+                <span>HP</span>
+                ${hp}
+            </p>
+            <img src=${imgSrc} />
+            <h2 class="poke-name">${pokeName}</h2>
+            <div class="types">
+            </div>
+            <div class="stats">
+                <div>
+                    <h3>${statAttack}</h3>
+                    <p>Attack</p>
+                </div><div>
+                    <h3>${statDeffense}</h3>
+                    <p>Defense</p>
+                </div><div>
+                    <h3>${statSpeed}</h3>
+                    <p>Speed</p>
+                </div>
+            </div>`
+            appendTypes(data.types)
+        }
+        let appendTypes = (types) => {
+            types.forEach((item) => {
+                let span = document.createElement("SPAN")
+                span.textContent = item.type.name
+                document.querySelector(".types").appendChild(span)
+            })
+        }
+
+
+btn.addEventListener("click", getPokeData)
+window.addEventListener("load", getPokeData)
+
